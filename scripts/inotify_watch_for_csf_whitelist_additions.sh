d44e5821d9ff020ab5b40fc61a583adf2b8a7abf
Gabriel Serafini <gserafini@gmail.com>
2015-08-03 14:26:13 -0700

    Adding instructions for setting up chkserv config for service

--- scripts/inotify_watch_for_csf_whitelist_additions.sh
+++ scripts/inotify_watch_for_csf_whitelist_additions.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Watch /etc/csf/csf.allow for new whitelist additions, restart apache whenever a new one is added
+
+# Watch this script using chkserv
+# echo 'service[inotify_watch_for_csf_whitelist_additions]=x,x,x,/root/inotify_watch_for_csf_whitelist_additions.sh,inotify_watch_for_csf_whitelist_additions.sh,root' > /etc/chkserv.d/inotify_csf_whitelist_monitor
+# /scripts/restartsrv_chkservd
+
 while true; do
   inotifywait -e modify /etc/csf/csf.allow && echo 'Restarting Apache...'; service httpd restart
 done
