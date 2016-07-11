3ed1ac34ea02845776cfafa4b2aa72c79c9d3eea
Gabriel Serafini <gserafini@gmail.com>
2016-07-11 00:24:34 -0700

    Updating svn_add_remove script to work better when signed in as root. Adding new fix_permissions script and additional inotify script

---
+++ scripts/inotify_watch_for_csf_whitelist_requests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Watch /etc/csf/csf.allow for new whitelist additions, restart apache whenever a new one is added
+
+# Watch this script using chkserv
+# echo 'service[inotify_watch_for_csf_whitelist_requests]=x,x,x,/root/inotify_watch_for_csf_whitelist_requests.sh,inotify_watch_for_csf_whitelist_requests.sh,root' > /etc/chkserv.d/inotify_csf_whitelist_requests_monitor
+# /scripts/restartsrv_chkservd
+
+while true; do
+  inotifywait -e modify /home/serafini/public_html/version-checker/requested_ips_for_whitelisting.txt && echo 'Got whitelist request from WhitelistMyIP.com'; /bin/bash /home/serafini/public_html/version-checker/requested_ips_for_whitelisting.txt
+done
+
