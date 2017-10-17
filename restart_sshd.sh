dc9782fadc5a886dbf4179896e458da5b6d20666
Gabriel Serafini <gserafini@gmail.com>
2017-10-17 10:13:46 -0700

    Make CentOS 7 also restart.  Not the best way to do this, but should work...

--- restart_sshd.sh
+++ restart_sshd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Restart sshd by killing all old / stale connections
 
-screen -D -m -S RestartSSHD bash -c 'killall sshd; sleep 5; /etc/init.d/sshd restart'
+screen -D -m -S RestartSSHD bash -c 'killall sshd; sleep 5; /etc/init.d/sshd restart; systemctl restart sshd.service'
 
