2968afeb84b77446d73ae844bb965c1b7798e559
Gabriel Serafini <gserafini@gmail.com>
2018-05-15 20:52:31 -0700

    Updating for @ sign in svn files

--- scripts/kill_long_php.sh
+++ scripts/kill_long_php.sh
@@ -2,6 +2,6 @@
 
 # This script will kill all php processes that have been running longer than 1 hour.
 
-ps -eo uid,pid,command,etime | grep php | egrep ' ([0-9]+-)?([0-9]{2}:?){3}' | awk '{print $2}' | xargs -I{} kill {}
+ps -eo uid,pid,command,etime | grep php | egrep ' ([0-9]+-)?([0-9]{2}:?){3}' | awk '{print $2}' | xargs -I % kill -9 %
 
 
